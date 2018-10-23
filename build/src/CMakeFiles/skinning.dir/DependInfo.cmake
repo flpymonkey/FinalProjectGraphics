3ae94db4affefc16c64fdc4d6ef78a0e37d435e4
Roger Marshe <marshe@cs.utexas.edu>
2018-10-23 16:05:15 -0500

    added some bone code and gitignore

---
+++ build/src/CMakeFiles/skinning.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/marshe/Desktop/skinner/src/bone_geometry.cc" "/u/marshe/Desktop/skinner/build/src/CMakeFiles/skinning.dir/bone_geometry.cc.o"
+  "/u/marshe/Desktop/skinner/src/gui.cc" "/u/marshe/Desktop/skinner/build/src/CMakeFiles/skinning.dir/gui.cc.o"
+  "/u/marshe/Desktop/skinner/src/main.cc" "/u/marshe/Desktop/skinner/build/src/CMakeFiles/skinning.dir/main.cc.o"
+  "/u/marshe/Desktop/skinner/src/procedure_geometry.cc" "/u/marshe/Desktop/skinner/build/src/CMakeFiles/skinning.dir/procedure_geometry.cc.o"
+  "/u/marshe/Desktop/skinner/src/render_pass.cc" "/u/marshe/Desktop/skinner/build/src/CMakeFiles/skinning.dir/render_pass.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  "/lusr/opt/glfw-3.2.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/u/marshe/Desktop/skinner/build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/bone_geometry.cc
+++ src/bone_geometry.cc
@@ -46,6 +46,39 @@ void Mesh::loadpmd(const std::string& fn)
 
 	// FIXME: load skeleton and blend weights from PMD file
 	//        also initialize the skeleton as needed
+
+	int bone_id = 0;
+	int parent_id;
+	glm::vec3 offset;
+	bool useful_bone;
+
+	while (useful_bone) {
+		useful_bone = mr.getJoint(bone_id++, offset, parent_id);
+		printf("bone_id: %d\n", bone_id);
+		printf("offset: (%f, %f, %f)\n", offset.x, offset.y, offset.z);
+		printf("parent_id: %d\n", parent_id);
+
+		Bone* bone = new Bone;
+
+		if (parent_id == -1) {
+			glm::vec3 parent_offset = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 tangent = glm::normalize(parent_offset - offset);
+			printf("tangent: (%f, %f, %f)\n", tangent.x, tangent.y, tangent.z);
+
+			glm::vec3 normal = computeNormal(tangent);
+			printf("normal: (%f, %f, %f)\n", normal.x, normal.y, normal.z);
+
+			glm::vec3 binormal = glm::cross(tangent, normal);
+			printf("binormal: (%f, %f, %f)\n", binormal.x, binormal.y, binormal.z);
+
+			bone->id = bone_id;
+			bone->parent_id = parent_id;
+		} else {
+			bone->id = bone_id;
+			bone->parent_id = parent_id;
+		}
+
+	}
 }
 
 void Mesh::updateAnimation()
@@ -64,5 +97,36 @@ void Mesh::computeBounds()
 		bounds.min = glm::min(glm::vec3(vert), bounds.min);
 		bounds.max = glm::max(glm::vec3(vert), bounds.max);
 	}
+}
+
+glm::vec3 Mesh::computeNormal(glm::vec3 tangent)
+{
+	glm::vec3 v = glm::normalize(tangent);
+	v.x = glm::abs(v.x);
+	v.z = glm::abs(v.z);
+	v.y = glm::abs(v.y);
+
+	float min = glm::min(v.x, glm::min(v.y, v.z));
+	if (min == v.x) {
+		v.x = 1.0f;
+		v.y = 0.0f;
+		v.z = 0.0f;
+	}
+	if (min == v.y) {
+		v.x = 0.0f;
+		v.y = 1.0f;
+		v.z = 0.0f;
+	}
+	if (min == v.z) {
+		v.x = 0.0f;
+		v.y = 0.0f;
+		v.z = 1.0f;
+	}
+
+	printf("v: (%f, %f, %f)\n", v.x, v.y, v.z);
+
+	glm::vec3 nominator = glm::cross(tangent, v);
+	float denominator = glm::length(nominator);
+	return nominator / denominator;
 }
 
