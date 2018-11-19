cd422f44e7b65a3e7cfe98c9062c95e3ec35cde2
Benjamin Johnson <bencj96@gmail.com>
2018-11-19 15:30:32 -0600

    Imported assimp

--- src/main.cc
+++ src/main.cc
@@ -13,6 +13,12 @@
 // OpenGL library includes
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+// Include AssImp
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
 #include "debuggl.h"
 
 // Classes
