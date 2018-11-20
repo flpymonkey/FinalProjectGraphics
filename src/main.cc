a0c0fa87f963d3cc1ef6d7e63f962f1ab3875321
Mitchell Roger Marshe <mitchellmarshe@gmail.com>
2018-11-20 14:41:51 -0600

    added cmake for shaders, seems to work...

--- src/main.cc
+++ src/main.cc
@@ -73,240 +73,25 @@ GLuint g_buffer_objects[kNumVaos][kNumVbos];  // These will store VBO descriptor
 
 // Shaders
 const char* vertex_shader =
-R"zzz(#version 330 core
-in vec4 vertex_position;
-in vec4 vertex_normal;
-uniform mat4 view;
-uniform mat4 projection;
-uniform vec4 light_position;
-out vec4 light_direction;
-out vec4 normal;
-out vec4 world_normal;
-out vec4 world_position;
-void main()
-{
-// Transform vertex into clipping coordinates
-	gl_Position = projection * view * vertex_position;
-// Lighting in camera coordinates
-//  Compute light direction and transform to camera coordinates
-        light_direction = view * (light_position - vertex_position);
-//  Transform normal to camera coordinates
-        normal = view * vertex_normal;
-        world_normal = vertex_normal;
-        world_position = projection * view * vertex_position;
-}
-)zzz"
+#include "shaders/default.vert"
 ;
 
 const char* fragment_shader =
-R"zzz(#version 330 core
-
-// Source: https://learnopengl.com/Lighting/Multiple-lights
-
-struct Material {
-    sampler2D diffuse;
-    sampler2D specular;
-    float shininess;
-}; 
-
-struct DirectionalLight {
-    vec3 direction;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-};
-
-struct PointLight {
-    vec3 position;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float constant;
-    float linear;
-    float quadratic;
-};
-
-struct SpotLight {
-    vec3 position;
-    vec3 direction;
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular; 
-    float constant;
-    float linear;
-    float quadratic;
-    float cutOff;
-    float outerCutOff;
-};
-
-in vec4 normal;
-in vec4 light_direction;
-in vec4 world_normal;
-in vec4 world_position;
-
-uniform vec4 view_position;
-
-uniform int dLights;
-uniform int pLights;
-uniform int sLights;
-
-uniform DirectionalLight directionalLights[10];
-uniform PointLight pointLights[10];
-uniform SpotLight spotLights[10];
-
-out vec4 fragment_color;
-
-// calculates the color when using a directional light.
-vec3 CalcDirLight(DirectionalLight light, vec3 normal, vec3 viewDir)
-{
-    vec3 lightDir = normalize(-light.direction);
-    // diffuse shading
-    float diff = max(dot(normal, lightDir), 0.0);
-    // specular shading
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1.0); // material.shininess);
-    // combine results
-    vec3 ambient = light.ambient; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 diffuse = light.diffuse * diff; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 specular = light.specular * spec; // * vec3(texture(material.specular, TexCoords));
-    return (ambient + diffuse + specular);
-}
-
-// calculates the color when using a point light.
-vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
-{
-    vec3 lightDir = normalize(light.position - fragPos);
-    // diffuse shading
-    float diff = max(dot(normal, lightDir), 0.0);
-    // specular shading
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1.0); // material.shininess);
-    // attenuation
-    float distance = length(light.position - fragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
-    // combine results
-    vec3 ambient = light.ambient; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 diffuse = light.diffuse * diff; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 specular = light.specular * spec; // * vec3(texture(material.specular, TexCoords));
-    ambient *= attenuation;
-    diffuse *= attenuation;
-    specular *= attenuation;
-    return (ambient + diffuse + specular);
-}
-
-// calculates the color when using a spot light.
-vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
-{
-    vec3 lightDir = normalize(light.position - fragPos);
-    // diffuse shading
-    float diff = max(dot(normal, lightDir), 0.0);
-    // specular shading
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 1.0); // material.shininess);
-    // attenuation
-    float distance = length(light.position - fragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
-    // spotlight intensity
-    float theta = dot(lightDir, normalize(-light.direction)); 
-    float epsilon = light.cutOff - light.outerCutOff;
-    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
-    // combine results
-    vec3 ambient = light.ambient; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 diffuse = light.diffuse * diff; // * vec3(texture(material.diffuse, TexCoords));
-    vec3 specular = light.specular * spec; // * vec3(texture(material.specular, TexCoords));
-    ambient *= attenuation * intensity;
-    diffuse *= attenuation * intensity;
-    specular *= attenuation * intensity;
-    return (ambient + diffuse + specular);
-}
-
-void main()
-{
-	vec4 color = abs(normalize(world_normal)) + vec4(0.0, 0.0, 0.0, 1.0);
-
-	vec3 norm = vec3(normalize(normal));
-	vec3 viewDir = normalize(vec3(view_position) - vec3(world_position));
-
-    for (int dLight = 0; dLight < dLights; dLight++) {
-        fragment_color += vec4(CalcDirLight(directionalLights[dLight], norm, viewDir), 1.0);
-    }
-
-    for (int pLight = 0; pLight < pLights; pLight++) {
-	   fragment_color += vec4(CalcPointLight(pointLights[pLight], norm, vec3(world_position), viewDir), 1.0);
-    }
-
-    for (int sLight = 0; sLight < sLights; sLight++) {
-        fragment_color += vec4(CalcSpotLight(spotLights[sLight], norm, vec3(world_position), viewDir), 1.0);
-    }
-
-    fragment_color *= color;
-}
-)zzz"
+#include "shaders/default.frag"
 ;
 
 const char* floor_fragment_shader =
-R"zzz(#version 330 core
-in vec4 normal;
-in vec4 light_direction;
-in vec4 world_position;
-uniform mat4 view;
-uniform mat4 projection;
-uniform vec4 light_position;
-out vec4 fragment_color;
-void main()
-{
-	vec4 position = inverse(projection * view) * vec4(world_position.xyz / world_position.w, 1.0f);
-    position /= position.w;
-	vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
-    if (mod(floor(position[0]) + floor(position[2]), 2) == 0) {
-        color = vec4(1.0, 1.0, 1.0, 1.0);
-    }
-	float dot_nl = dot(normalize(light_direction), normalize(normal));
-	dot_nl = clamp(dot_nl, 0.0, 1.0);
-	fragment_color = clamp(dot_nl * color, 0.0, 1.0);
-}
-)zzz"
+#include "shaders/floor.frag"
 ;
 
 const char* screen_vertex_shader =
-R"zzz(#version 330 core
-in vec2 vertex_position;
-in vec2 aTexCoords;
-out vec2 TexCoords;
-void main()
-{
-    TexCoords = aTexCoords;
-    gl_Position = vec4(vertex_position.x, vertex_position.y, 0.0, 1.0);
-}
-)zzz"
-
+#include "shaders/screen_default.vert"
 ;
 
 // hdr shader which uses Reinhard tone mapping for high dynamic range
 const char* screen_fragment_shader =
-R"zzz(#version 330 core
-out vec4 fragment_color;
-in vec2 TexCoords;
-uniform sampler2D screenTexture;
-
-uniform float exposure;
-
-// hdr shader which uses Reinhard tone mapping for high dynamic range
-void main()
-{
-		const float gamma = 2.2;
-    vec3 hdrColor = texture(screenTexture, TexCoords).rgb;
-
-		// Exposure tone mapping
-    vec3 mapped = vec3(1.0) - exp(-hdrColor * exposure);
-    // gamma correction
-    mapped = pow(mapped, vec3(1.0 / gamma));
-
-    fragment_color = vec4(mapped, 1.0);
-}
-)zzz"
+#include "shaders/screen_hdr.frag"
 ;
-
 
 void
 ErrorCallback(int error, const char* description)
