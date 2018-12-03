14d72a462a3b579b618ba7c09414ca18594b1618
flpymonkey <bencj@cs.utexas.edu>
2018-12-03 10:20:58 -0600

    Added compile version for windows that excludes screenshot functionality

--- src/jpegio.h
+++ src/jpegio.h
@@ -8,6 +8,5 @@ bool SaveJPEG(const std::string& filename,
               int image_width,
               int image_height,
               const unsigned char* pixels);
-bool LoadJPEG(const std::string& file_name, Image* image);
 
 #endif
