5faeac20b7c8ad31d63031573985465dce729e06
flpymonkey <bencj@cs.utexas.edu>
2018-12-04 14:35:05 -0600

    Added GUI support

--- src/gui.cc
+++ src/gui.cc
@@ -2,13 +2,16 @@
 
 std::string glsl_version = "#version 330 core";
 ImVec4 clear_color = ImColor(114, 144, 154);
-bool show_test_window = false;
+bool show_test_window = true;
 
 BasicGUI::BasicGUI(GLFWwindow* window){
   // Setup ImGui binding
   if (show_test_window)
   {
-    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui_ImplGlfwGL3_Init(window, false);
+    auto io = ImGui::GetIO();
+    io.WantCaptureKeyboard = false;
+
   }
   this->window = window;
 }
@@ -27,7 +30,7 @@ void BasicGUI::render(){
     if (ImGui::Button("Test Window")) show_test_window ^= 1;
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 
-    ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+    ImGui::SetNextWindowPos(ImVec2(300, 5), ImGuiSetCond_FirstUseEver);
     ImGui::ShowTestWindow(&show_test_window);
     // Rendering
     ImGui::Render();
